9af0826ee10e2f3cf78dd9e16c6e94ddeb2b211f
ophiran <ophiran@hotmail.com>
2013-09-19 20:47:40 +0200

    Modif PartsType

--- src/bddDataObjects/PartsType.java
+++ src/bddDataObjects/PartsType.java
@@ -4,16 +4,37 @@
  */
 package bddDataObjects;
 
+import java.awt.List;
+
 /**
  *
  * @author mike
  */
 public enum PartsType {
-    L("l"), T("t"), U("u"), X2("x2"), X3("x3"), C2("c2"), C3("c3"), C4("c4")
-        ,T2("t2");
-    String type;
+    L("l",2000), 
+    T("t",5000), 
+    U("u",2700), 
+    X2("x2",3354), 
+    X3("x3",1457), 
+    C2("c2",2114), 
+    C3("c3",4521), 
+    C4("c4",4122),
+    T2("t2",4525);
     
-    PartsType(String type){
+    private String type;
+    private int baseTime;
+    
+    PartsType(String type,int baseTime){
         this.type = type;
+        this.baseTime = baseTime;
     }
+    
+    public String getType() {
+    	return type;
+    }
+    
+    public int getBaseTime() {
+    	return baseTime;
+    }
+    
 }
