8139da41d3282774045efd6a231170caa2ce5d4a
Mike David <m.davidmike@gmail.com>
2013-09-25 15:33:18 +0200

    Modif testBean

--- src/testBean.java
+++ src/testBean.java
@@ -1,6 +1,8 @@
 import java.sql.SQLException;
 
 import bddAccessObjects.BeanBDAccessCSV;
+import bddAccessObjects.BeanBDAccessMysql;
+import java.sql.ResultSet;
 
 
 public class testBean {
@@ -10,20 +12,34 @@ public class testBean {
      */
     public static void main(String[] args) {
        
-       
+       System.out.println("Trying to connect...");
         try {
-            BeanBDAccessCSV bean = new BeanBDAccessCSV();
-            bean.startConnection("/127.0.0.1:3306/mydb");
-            Thread.sleep(2000);
+            /*
+            BeanBDAccessMysql bean = new BeanBDAccessMysql();
+            bean.startConnection("//127.0.0.1:3306/mydb", "root", "");
+            System.out.println("Connection established");
+            ResultSet rs = bean.sendQuery("select * from parts");
+            while(rs.next()){
+                System.out.println(((Integer)rs.getInt("FabricationTime")).toString());
+            }
             bean.stopConnection();
+            System.out.println("Deconnected");
+            */
+            BeanBDAccessCSV bean = new BeanBDAccessCSV();
+            bean.startConnection("c:\\mydb");
+            System.out.println("Connection established");
+            ResultSet rs = bean.sendQuery("select * from parts");
+            while(rs.next()){
+                System.out.println(((Integer)rs.getInt("FabricationTime")).toString());
+            }
+            bean.stopConnection();
+            System.out.println("Deconnected");
             
         } catch (SQLException e) {
             // TODO Auto-generated catch block
             e.printStackTrace();
         } catch (ClassNotFoundException cnfe){
-           
-        } catch (InterruptedException ie) {
-            
+           cnfe.printStackTrace();
         }
         
 
