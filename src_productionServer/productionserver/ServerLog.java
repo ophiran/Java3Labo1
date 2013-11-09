3b231707fbb01aaafae290b855eda0885cc09499
miked <m.davidmike@gmail.com>
2013-11-09 08:04:57 +0100

    Adds ServerLauncher and ServerLog

---
+++ src_productionServer/productionserver/ServerLog.java
@@ -0,0 +1,15 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package productionserver;
+
+/**
+ *
+ * @author mike
+ */
+public class ServerLog {
+    public static void write(String message) {
+        System.out.println(message);
+    }
+}
